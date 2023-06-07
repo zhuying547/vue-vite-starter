8d790ebeb4184c7235349865f6f7ed41700b9a38
zhuying547 <zhuying547@qq.com>
2023-06-07 23:14:39 +0800

    refactor: 上ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import path from 'path'
+
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { TDesignResolver } from 'unplugin-vue-components/resolvers'
+
 export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': path.resolve(__dirname, 'src/'),
+    },
+  },
   plugins: [
     vue(),
     vueJsx(),
@@ -24,9 +31,4 @@ export default defineConfig({
       ],
     }),
   ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
 })
