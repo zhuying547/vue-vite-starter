7d2c3d9a0605c5b47812a16e6d7eb22081b68407
zhuying <zhuying547@qq.com>
2023-08-08 14:27:14 +0800

    style: follow the latest create-vue

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
-import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+import path from 'path'
 
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { TDesignResolver } from 'unplugin-vue-components/resolvers'
-
-import { fileURLToPath, URL } from 'node:url'
-import path from 'path'
 
 export default defineConfig({
   base: '/vue-vite-starter/',
